2f81afc83400c5eed7bf65092f927b999ae20086
Gerald Encabo <geraldencabo@gmail.com>
2023-02-27 22:16:04 -0500

    Update

--- frontend/src/pages/Services/Services.js
+++ frontend/src/pages/Services/Services.js
@@ -12,42 +12,42 @@ function Services() {
                     <div className="services-container-cardInfo">
                         <img src={process.env.PUBLIC_URL + '/images/svg-services-1.svg'} className="services-container-img" alt="Managed Services display"/>
                         <h3 className="services-heading-subtitle">Managed Services</h3>
-                        <p className="services-text">Unleash the power of insights with critical business execution across campaigns and channels.</p>
-                        </div>
+                        <p className="services-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente voluptates molestiae incidunt praesentium autem provident.</p>
+                      </div>
               </Link>
               <Link to="#" className="services-container-card">
                     <div className="services-container-cardInfo">
                         <img src={process.env.PUBLIC_URL + '/images/svg-services-2.svg'} className="services-container-img" alt="Financial Services display"/>
                         <h3 className="services-heading-subtitle">Financial Services</h3>
-                        <p className="services-text">Continuously innovate and optimize your programs to meet changing consumer financial needs.</p>
+                        <p className="services-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae magnam vitae veritatis dolores molestiae corporis, numquam ducimus.</p>
                     </div>
               </Link>
               <Link to="#" className="services-container-card">
                     <div className="services-container-cardInfo">
                         <img src={process.env.PUBLIC_URL + '/images/svg-services-3.svg'} className="services-container-img" alt="Cardholder Services display"/>
                         <h3 className="services-heading-subtitle">Cardholder Services</h3>
-                        <p className="services-text">Drive brand loyalty and card preference with cardholder services designed to fit consumer needs.</p>
+                        <p className="services-text">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Rerum, exercitationem molestias alias totam eaque blanditiis.</p>
                     </div>
               </Link>
               <Link to="#" className="services-container-card">
                     <div className="services-container-cardInfo">
                         <img src={process.env.PUBLIC_URL + '/images/svg-services-4.svg'} className="services-container-img" alt="Authentication Services display"/>
                         <h3 className="services-heading-subtitle">Authentication Services</h3>
-                        <p className="services-text">State-of-the-art, multi-layered authentication, designed for the unique behavior of every person.</p>  
+                        <p className="services-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quam nulla, modi nam quos ab vel ullam vero.</p>  
                     </div>
               </Link>
               <Link to="#" className="services-container-card">
                     <div className="services-container-cardInfo">
                         <img src={process.env.PUBLIC_URL + '/images/svg-services-5.svg'} className="services-container-img" alt="Financial Services display"/>
                         <h3 className="services-heading-subtitle">ID Theft Protection Services</h3>
-                        <p className="services-text">Customers can monitor their identity 24/7 and respond to suspicious activity from our easy-to-use online platform.</p>
+                        <p className="services-text">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Qui quam nobis obcaecati provident repellendus.</p>
                     </div>
               </Link>
               <Link to="#" className="services-container-card">
                     <div className="services-container-cardInfo">
                         <img src={process.env.PUBLIC_URL + '/images/svg-services-6.svg'} className="services-container-img" alt="Cardholder Services display"/>
                         <h3 className="services-heading-subtitle">Cyber Security Services</h3>
-                        <p className="services-text">Take advantage of tips and resources designed to help protect your business in our data-dependent, connected world.</p>
+                        <p className="services-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi necessitatibus repellendus ipsa laboriosam at ratione.</p>
                     </div>
               </Link>
          </div>
